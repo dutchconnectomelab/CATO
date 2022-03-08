57304d34eb1bfab36a73bff38008bd33370ba880
Siemon de Lange <SiemondeLange@users.noreply.github.com>
2022-03-08 15:06:15 +0100

    Reduce structural pipeline dependencies
    Require only the statistics toolbox. Remove the usage of `struct2array` which is not always available in MATLAB.

--- src/structural_pipeline/structural_pipeline.m
+++ src/structural_pipeline/structural_pipeline.m
@@ -54,13 +54,11 @@ end
 
 % CATO uses statistics and signal toolbox.
 license('checkout','statistics_toolbox');
-license('checkout','signal_toolbox');
-assert(contains(struct2array(ver), ...
+toolboxInstalled = ver;
+assert(contains([toolboxInstalled.Name], ...
     'Statistics and Machine Learning Toolbox'), ...
     'Statistics and Machine Learning Toolbox must be installed.');
-assert(contains(struct2array(ver), ...
-    'Signal Processing Toolbox'), ...
-    'Signal Processing Toolbox must be installed.');
+clear toolboxInstalled
 
 % PARSE INPUT
 [subjectDir, varargin{:}] = convertStringsToChars(subjectDir, varargin{:});
